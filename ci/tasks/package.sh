5885d3bf18a7ae339b18ea2deb4397529f7f5bd7
Sundaresancts <sundaresan.kumbeswaran@cognizant.com>
2017-10-12 17:18:05 +0530

    Change package.sh

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -7,7 +7,7 @@ pushd attendee-service
   ./mvnw clean package -DskipTests
 popd
 
-jar_count=`find attendee-service/target -type f -name *.jar | wc -l`
+jar_count=`find attendee-service-source/target -type f -name *.jar | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting"
